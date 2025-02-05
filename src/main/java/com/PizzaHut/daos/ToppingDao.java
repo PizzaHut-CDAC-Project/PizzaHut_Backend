3ee92c4713a82f3d08a20803775794c988027c89
PrathameshDeshpande6546 <prathameshdeshpande68@gmail.com>
2025-02-05 14:56:16 +0530

    Topping Get Api Added

---
+++ src/main/java/com/PizzaHut/daos/ToppingDao.java
@@ -0,0 +1,13 @@
+package com.PizzaHut.daos;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.PizzaHut.entities.Topping;
+
+
+public interface ToppingDao extends JpaRepository<Topping, Integer>{
+	
+	
+	
+}
