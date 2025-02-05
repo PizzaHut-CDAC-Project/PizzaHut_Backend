405f4edd806c3e7b47dba063b84580aca46c4495
Pratik Korane <pratik894720@gmail.com>
2025-02-05 19:13:55 +0530

    delivery status by user Id

---
+++ src/main/java/com/PizzaHut/daos/DeliveryStatusDao.java
@@ -0,0 +1,15 @@
+package com.PizzaHut.daos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.PizzaHut.entities.DeliveryStatus;
+import com.PizzaHut.entities.User;
+
+public interface DeliveryStatusDao extends JpaRepository<DeliveryStatus	,Integer>{
+	List<DeliveryStatus> findByUser(User user);
+	
+	 
+}
