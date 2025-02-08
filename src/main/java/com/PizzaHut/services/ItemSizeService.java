abcd21e5b84dc5c6484e0ffdce1033053d37cf1b
Indrajeet Shinde <iamindrajeets242@gmail.com>
2025-02-08 23:39:18 +0530

    Show itemSize by item Id API added

---
+++ src/main/java/com/PizzaHut/services/ItemSizeService.java
@@ -0,0 +1,39 @@
+package com.PizzaHut.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.PizzaHut.daos.ItemDao;
+import com.PizzaHut.daos.ItemSizeDao;
+import com.PizzaHut.dtos.ItemSizeDto;
+import com.PizzaHut.entities.ItemSize;
+import com.app.custom_exceptions.ResourceNotFoundException;
+
+@Service
+@Transactional
+public class ItemSizeService {
+
+	@Autowired
+	private ItemSizeDao itemSizeDao;
+	@Autowired
+	private ItemDao itemDao;
+
+	// show by sizeId
+	public List<ItemSize> SizeOfPizza(int itemId) {
+		List<ItemSize> thisSize = itemSizeDao.getSizeOfPizza(itemId);
+		return thisSize;
+	}
+
+	public ItemSize addItemSize(ItemSizeDto itemSizeDto) {
+		ItemSize itemSize = new ItemSize();
+		itemSize.setItem(itemDao.findById(itemSizeDto.getItemId()).orElseThrow(
+				() -> new ResourceNotFoundException("item with " + itemSizeDto.getItemId() + " not available")));
+		itemSize.setPrice(itemSizeDto.getPrice());
+		itemSize.setSize(itemSizeDto.getSize());
+		itemSizeDao.save(itemSize);
+		return itemSize;
+	}
+}
